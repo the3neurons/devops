41589bcfe2ce6cfa32265d184d809f45c6ec8ebc
opixelum <48217573+opixelum@users.noreply.github.com>
2025-07-23 17:12:13 +0200

    refactor(tf): remove mysql

--- main.tf
+++ main.tf
@@ -1,10 +1,3 @@
-resource "random_string" "suffix" {
-  length  = 8
-  special = false
-  upper   = false
-  numeric = true
-}
-
 resource "azurerm_resource_group" "rg" {
   name     = var.rg-name
   location = var.location
@@ -19,82 +12,7 @@ resource "azurerm_storage_account" "sa" {
 }
 
 resource "azurerm_storage_container" "training-data" {
-  name                  = "training-data"
+  name                  = var.sc-name
   storage_account_id    = azurerm_storage_account.sa.id
   container_access_type = "private"
-}
-
-resource "azurerm_linux_web_app" "app" {
-  name                = "app-${random_string.suffix.result}"
-  location            = azurerm_resource_group.rg.location
-  resource_group_name = azurerm_resource_group.rg.name
-  service_plan_id     = azurerm_service_plan.asp.id
-
-  app_settings = {
-    MYSQL_HOST                     = azurerm_mysql_flexible_server.mysql_server.fqdn
-    MYSQL_USER                     = var.mysql_user
-    MYSQL_PASSWORD                 = var.mysql_password
-    MYSQL_DATABASE                 = var.mysql_dbname
-    SCM_DO_BUILD_DURING_DEPLOYMENT = "true"
-  }
-
-  site_config {
-    application_stack {
-      node_version = "22-lts"
-    }
-
-    always_on = true
-
-    cors {
-      allowed_origins = ["*"]
-    }
-  }
-
-  ftp_publish_basic_authentication_enabled       = true
-  webdeploy_publish_basic_authentication_enabled = true
-
-  identity {
-    type = "SystemAssigned"
-  }
-}
-
-resource "azurerm_mysql_flexible_server" "mysql_server" {
-  name                   = var.mysql_server_name
-  resource_group_name    = azurerm_resource_group.rg.name
-  location               = var.location
-  administrator_login    = var.mysql_user
-  administrator_password = var.mysql_password
-  version                = "8.0.21"
-  sku_name               = "B_Standard_B1ms"
-}
-
-resource "azurerm_mysql_flexible_server_configuration" "require_secure_transport" {
-  name                = "require_secure_transport"
-  resource_group_name = azurerm_mysql_flexible_server.mysql_server.resource_group_name
-  server_name         = azurerm_mysql_flexible_server.mysql_server.name
-  value               = "OFF"
-}
-
-resource "azurerm_mysql_flexible_server_firewall_rule" "allow_azure_services" {
-  name                = "AllowAzureServices"
-  resource_group_name = azurerm_resource_group.rg.name
-  server_name         = azurerm_mysql_flexible_server.mysql_server.name
-  start_ip_address    = "0.0.0.0"
-  end_ip_address      = "0.0.0.0"
-}
-
-resource "azurerm_service_plan" "asp" {
-  name                = "asp-${random_string.suffix.result}"
-  location            = azurerm_resource_group.rg.location
-  resource_group_name = azurerm_resource_group.rg.name
-  os_type             = "Linux"
-  sku_name            = "B2"
-}
-
-resource "azurerm_mysql_flexible_database" "mysql_db" {
-  name                = var.mysql_dbname
-  resource_group_name = azurerm_resource_group.rg.name
-  server_name         = azurerm_mysql_flexible_server.mysql_server.name
-  charset             = "utf8"
-  collation           = "utf8_unicode_ci"
 }
--- variables.tf
+++ variables.tf
@@ -16,26 +16,8 @@ variable "sa-name" {
   default     = "stgthe3neurons"
 }
 
-variable "mysql_user" {
-  description = "MySQL username"
+variable "sc-name" {
+  description = "Storage container name"
   type = string
-  default = "fake_user"
-}
-
-variable "mysql_password" {
-  description = "MySQL password"
-  type = string
-  default = "Fake_password_0"
-}
-
-variable "mysql_dbname" {
-  description = "MySQL database name"
-  type = string
-  default = "fake-dbname"
-}
-
-variable "mysql_server_name" {
-  description = "MySQL server name"
-  type = string
-  default = "fake-server-name"
+  default = "training-data"
 }
