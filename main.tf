11bb630b9b355cd9630724440b9e7c4e74582fe7
opixelum <48217573+opixelum@users.noreply.github.com>
2025-07-23 10:35:35 +0200

    fix(tf): add network config

--- main.tf
+++ main.tf
@@ -76,10 +76,38 @@ resource "azurerm_machine_learning_workspace" "ml-ws" {
   }
 }
 
-resource "azurerm_network_interface" "ni" {
-  name                = "ni${random_string.suffix.result}"
+resource "azurerm_virtual_network" "vnet" {
+  name                = "vnet-${random_string.suffix.result}"
+  address_space       = ["10.0.0.0/16"]
   location            = azurerm_resource_group.rg.location
   resource_group_name = azurerm_resource_group.rg.name
+}
+
+resource "azurerm_subnet" "subnet" {
+  name                 = "subnet-${random_string.suffix.result}"
+  resource_group_name  = azurerm_resource_group.rg.name
+  virtual_network_name = azurerm_virtual_network.vnet.name
+  address_prefixes     = ["10.0.1.0/24"]
+}
+
+resource "azurerm_public_ip" "pubip" {
+  name                = "pip-${random_string.suffix.result}"
+  location            = azurerm_resource_group.rg.location
+  resource_group_name = azurerm_resource_group.rg.name
+  allocation_method   = "Dynamic"
+}
+
+resource "azurerm_network_interface" "ni" {
+  name                = "ni-${random_string.suffix.result}"
+  location            = azurerm_resource_group.rg.location
+  resource_group_name = azurerm_resource_group.rg.name
+
+  ip_configuration {
+    name                          = "ipconfig1"
+    subnet_id                     = azurerm_subnet.subnet.id
+    private_ip_address_allocation = "Dynamic"
+    public_ip_address_id          = azurerm_public_ip.pubip.id
+  }
 }
 
 resource "azurerm_linux_virtual_machine" "vm" {
