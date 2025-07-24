dfc60ab12571b38342839a27cc371dff5e9deddd
jogoml <johanribl@gmail.com>
2025-07-24 13:30:37 +0200

    feat: modify main tf

--- python/backend/app.py
+++ python/backend/app.py
@@ -36,4 +36,5 @@ def predict():
     return jsonify({"prediction": label})
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=8000)
+    port = int(os.environ.get("PORT", 8000))
+    app.run(host="0.0.0.0", port=port)
